df8d076c1a024e74504fa609eebaec96c00a979e
Michael Lu <yeskky@gmail.com>
2023-10-31 22:03:40 +0800

    小时和分钟分开输入

--- app/src/main/java/free/bigflowertiger/cocassistant/ui/screen/SpeedCalcScreen.kt
+++ app/src/main/java/free/bigflowertiger/cocassistant/ui/screen/SpeedCalcScreen.kt
@@ -3,16 +3,20 @@ package free.bigflowertiger.cocassistant.ui.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -20,21 +24,42 @@ fun SpeedCalcScreen(
     viewModel: SpeedCalcViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    var input by remember { mutableStateOf("") }
+    var inputHour by remember { mutableStateOf("") }
+    var inputMinute by remember { mutableStateOf("") }
     Column(modifier = Modifier.padding(4.dp)) {
-        Row {
-            OutlinedTextField(
-                label = {
-                    Text(text = "研究时间（分钟）")
-                },
-                value = input,
-                onValueChange = {
-                    input = it
-                    viewModel.getActualResearchMinutes(it)
-                }
-            )
+        OutlinedTextField(
+            label = {
+                Text(text = "研究时间（小时）")
+            },
+            value = inputHour,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Number
+            ),
+            onValueChange = {
+                inputHour = it
+                viewModel.getActualResearchMinutes(inputHour, inputMinute)
+            }
+        )
+        OutlinedTextField(
+            label = {
+                Text(text = "研究时间（分钟）")
+            },
+            value = inputMinute,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Number
+            ),
+            onValueChange = {
+                inputMinute = it
+                viewModel.getActualResearchMinutes(inputHour, inputMinute)
+            }
+        )
 
-            Text(text = state.actualResearchMinutes.toString())
-        }
+        Text(
+            modifier = Modifier.padding(8.dp),
+            fontSize = 20.sp,
+            text = state.actualResearchMinutes.toString()
+        )
     }
 }
--- app/src/main/java/free/bigflowertiger/cocassistant/ui/screen/SpeedCalcViewModel.kt
+++ app/src/main/java/free/bigflowertiger/cocassistant/ui/screen/SpeedCalcViewModel.kt
@@ -11,8 +11,9 @@ class SpeedCalcViewModel @Inject constructor() : ViewModel() {
     private val _state = mutableStateOf(SpeedCalcState())
     val state: State<SpeedCalcState> = _state
 
-    fun getActualResearchMinutes(input: String) {
-        val input1 = input.toIntOrNull()
-        _state.value = _state.value.copy(actualResearchMinutes = input1?.let { it / 24 } ?: 0)
+    fun getActualResearchMinutes(hours: String, minutes: String) {
+        val input1 = hours.toIntOrNull() ?: 0
+        val input2 = minutes.toIntOrNull() ?: 0
+        _state.value = _state.value.copy(actualResearchMinutes = input1 * 60 / 24 + input2)
     }
 }
