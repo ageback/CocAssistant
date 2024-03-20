f7ec67cc554e0435fe575f6db44a10c351db7f6d
Michael Lu <yeskky@gmail.com>
2024-03-20 11:05:51 +0800

    edge to edge

--- app/src/main/java/free/bigflowertiger/cocassistant/MainActivity.kt
+++ app/src/main/java/free/bigflowertiger/cocassistant/MainActivity.kt
@@ -2,14 +2,25 @@ package free.bigflowertiger.cocassistant
 
 import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets.Type
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,12 +29,15 @@ import free.bigflowertiger.cocassistant.ui.theme.CocAssistantTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+//        WindowCompat.setDecorFitsSystemWindows(window,false)
+
         super.onCreate(savedInstanceState)
         setContent {
             CocAssistantTheme {
-
                 val locationPermissionState =
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                         rememberMultiplePermissionsState(
@@ -39,11 +53,10 @@ class MainActivity : ComponentActivity() {
                 }
 
                 if (locationPermissionState.allPermissionsGranted) {
-                    Surface(
+                    Scaffold(
                         modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        Column {
+                    ) { paddingValues ->
+                        Column(modifier = Modifier.padding(paddingValues)) {
                             SpeedCalcScreen()
                         }
                     }
