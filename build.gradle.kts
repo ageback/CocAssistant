7b75f29aa6e6e14eb9308c72e62505321c4debcd
Michael Lu <yeskky@gmail.com>
2024-03-20 10:46:43 +0800

    upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
 }
